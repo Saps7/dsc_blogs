e0f74e3e196c57d08bf962e756a59447c35a58d2
saptarshi <saptarshisg.2000@gmail.com>
2020-07-15 20:13:11 +0530

    Try

--- firebase/config.js
+++ firebase/config.js
@@ -1,12 +1,11 @@
  const firebaseConfig = {
-    apiKey: "AIzaSyDfqpLjCdb9tCdbCIFEI90LkXeEfeBUzs4",
-    authDomain: "dsc-nsec-blogs.firebaseapp.com",
-    databaseURL: "https://dsc-nsec-blogs.firebaseio.com",
-    projectId: "dsc-nsec-blogs",
-    storageBucket: "dsc-nsec-blogs.appspot.com",
-    messagingSenderId: "1066927664407",
-    appId: "1:1066927664407:web:1356f357100725c19e1b52",
-    measurementId: "G-HWP4E2BVED"
+  apiKey: "AIzaSyB1iVv9mohuKcZKUhLdf-EyMPX3_wa3q9E",
+  authDomain: "next-login-75d15.firebaseapp.com",
+  databaseURL: "https://next-login-75d15.firebaseio.com",
+  projectId: "next-login-75d15",
+  storageBucket: "next-login-75d15.appspot.com",
+  messagingSenderId: "1077136837953",
+  appId: "1:1077136837953:web:d5051bbca60f99fe9365c0"
   };
 
 export default firebaseConfig;
